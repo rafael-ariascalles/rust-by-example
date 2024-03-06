f990f394f1f3dda350d848a12d4256c53d10a446
Rafael Arias <45745870+rafael-ariascalles@users.noreply.github.com>
2024-03-06 14:46:06 +0000

    print statements

--- print_statement/src/main.rs
+++ print_statement/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("Hello, world!");
+
+    let name: &str = "poule";
+    let last_name: &str = "show";
+    println!("{name:#<10} and {last_name:#>10}");
+
+    #[derive(Debug)]
+    #[allow(dead_code)]
+    struct Person<'a> {
+        name: &'a str,
+        age: u8
+    }
+
+    let name: &str = "Peter";
+    let age: u8 = 27;
+    let peter = Person { name, age };
+    // Pretty print
+    println!("{:?}", peter);
 }
