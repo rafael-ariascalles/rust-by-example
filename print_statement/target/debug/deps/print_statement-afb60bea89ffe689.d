ffd29611a94516d659824fa857f31024e4f52ff6
Rafael Arias <45745870+rafael-ariascalles@users.noreply.github.com>
2024-03-06 14:18:27 +0000

    statement

---
+++ print_statement/target/debug/deps/print_statement-afb60bea89ffe689.d
@@ -0,0 +1,5 @@
+/workspaces/rust-by-example/print_statement/target/debug/deps/print_statement-afb60bea89ffe689: src/main.rs
+
+/workspaces/rust-by-example/print_statement/target/debug/deps/print_statement-afb60bea89ffe689.d: src/main.rs
+
+src/main.rs:
---
+++ print_statement/target/debug/print_statement.d
@@ -0,0 +1,1 @@
+/workspaces/rust-by-example/print_statement/target/debug/print_statement: /workspaces/rust-by-example/print_statement/src/main.rs
